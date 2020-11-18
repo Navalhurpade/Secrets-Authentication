5bf345a5bb01f451e7ecf893d1bfdedf27706342
Naval <naval@pop-os.localdomain>
2020-11-18 19:58:14 +0530

    Security Level 6--Using Cookies with Session

--- app.js
+++ app.js
@@ -1,36 +1,63 @@
 //jshint esversion:
-// require('dotenv').config()
+require('dotenv').config()
 // const md5 = require('md5');
+// const mongooseEncryption = require('mongoose-encryption');
+// const bcrypt = require('bcrypt')
+
+
 const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-// const mongooseEncryption = require('mongoose-encryption');
-const bcrypt = require('bcrypt');
-
+const passport = require('passport')
+const passportLocal = require('passport-local')
+const session = require('express-session')
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const app = express()
 app.set('view engine', 'ejs')
-app.use(express.static(__dirname + "/Public"))
+app.use(express.static(__dirname + "/public"))
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-mongoose.connect("mongodb://localhost:27017/UsersDB", {useNewUrlParser: true, useUnifiedTopology: true})
+// -----------------------Using session--------------//
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+
+// -----------------------Using passport--------------//
+app.use(passport.initialize())
+app.use(passport.session())
+
+// --------------Then connectiong the databases-------//
+mongoose.connect("mongodb://localhost:27017/UsersDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+})
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 })
 
+userSchema.plugin(passportLocalMongoose)
 
 const roundsOfSalting = 10
 
-//------------------------ Using DOTENV to store encryption key-------------------//
-// console.log(process.env.SECRET);
+
 // userSchema.plugin(mongooseEncryption, {secret: process.env.SECRET , encryptedFields: ["password"] })
 
 const User = new mongoose.model("user", userSchema)
+
+passport.use(User.createStrategy())
+
+// -----------Setting up cookie--------------//
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
 app.route("/")
   .get(function(req, res) {
@@ -42,59 +69,76 @@ app.route("/login")
   .get(function(req, res) {
     res.render("login")
   })
-
   .post(function(req, res) {
-    let email = req.body.username
-    let password = req.body.password
-    User.findOne({
-      email: email
-    }, function(err, foundUser) {
-      if (!err) {
-        if (!foundUser) {
-            console.log("Incorrect Credential !")
-          res.redirect("/login")
-        } else {
-          bcrypt.compare(password, foundUser.password, function(err, result){
-            if (result===true) {
-              res.render("secrets")
-            } else {
-                console.log(("Incorrect Credentials !!!\nPlease Try again"))
-              res.redirect("/login")
-            }
-          })
-        }
+    const userDetails = new User({
+      username: req.body.username,
+      password: req.body.password
+    })
+    req.login(userDetails, function(err) {
+      if (err) {
+        console.log("Incorrect Credentials !"+ err);
+        res.redirect("/login")
       } else {
-        console.log(err + "  \nERORR WHILE VRIFING CREDENTIALS\n");
+        passport.authenticate("local")(req, res, function() {
+          console.log("You reached login/Authenticating");
+          res.redirect("/secrets")
+        })
       }
     })
   })
+
 
 
 app.route("/register")
   .get(function(req, res) {
     res.render("register")
   })
-
   .post(function(req, res) {
-  let password = req.body.password
-   bcrypt.hash(password, roundsOfSalting, function (err, hash){
-     const newUser = new User({
-       email: req.body.username ,
-       password: hash
-     })
-     console.log(newUser+"\n IS Being saved ! !");
-     newUser.save(function(err) {
-       if (!err) {
-         console.log("Registration Sucusess !")
-         res.render("login")
-       } else {
-         console.log(err)
-       }
-     })
-   })
+    let password = req.body.password
+    User.register({
+      username: req.body.email
+    }, req.body.password, function(err, user) {
+      if (err) {
+        console.log(err)
+        res.redirect("/register")
+      } else {
+        passport.authenticate("local")(req, res,
+          function() {
+            console.log("You reached regeister/auth");
+            res.redirect("/secrets")
+          });
+      }
+    })
 
 
+
+
+
+    // bcrypt.hash(password, roundsOfSalting, function (err, hash){
+    //   const newUser = new User({
+    //     email: req.body.username ,
+    //     password: hash
+    //   })
+    //   console.log(newUser+"\n IS Being saved ! !");
+    //   newUser.save(function(err) {
+    //     if (!err) {
+    //       console.log("Registration Sucusess !")
+    //       res.render("login")
+    //     } else {
+    //       console.log(err)
+    //     }
+    //   })
+    // })
   })
+
+
+app.route("/secrets").get(function(req, res) {
+  if (req.isAuthenticated()) {
+    res.render("secrets")
+  } else {
+    res.redirect("/login")
+  }
+})
 
 app.listen(8080, function(req, res) {
   console.log("Server is running At Port 8080");
